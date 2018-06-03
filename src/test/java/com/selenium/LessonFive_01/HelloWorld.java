9ad4c844f616a346779244f32faffd016d04704a
11486 <11486@LAPTOP-UKNL6QE2>
2018-06-03 14:22:24 +0800

    LessonFive_01

---
+++ src/test/java/com/selenium/LessonFive_01/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.selenium.LessonFive_01;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+	@Test
+	public void helloworld() {
+		System.out.println("Helloworld");
+	}
+}
